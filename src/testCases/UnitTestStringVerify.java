867addadc6027930157c5936f44de6db1e5720f6
jahidakther <40965705+jahidakther@users.noreply.github.com>
2019-01-15 21:57:27 -0500

    Add files via upload

--- src/testCases/UnitTestStringVerify.java
+++ src/testCases/UnitTestStringVerify.java
@@ -8,16 +8,32 @@ import utilities.AllMethod;
 public class UnitTestStringVerify {
 	AllMethod am=new AllMethod();
 	
+  //Inject a string into the two method expected return value should be a integer and a string. 	
+  @Test
+  public void methodTesting() {
+	  String inputString="The cow jumped over the moon";
+	  int stringLenght= am.lenght(inputString);
+	  String longestWord=am.longestWord(inputString);
+	  System.out.println("input String lenght is :" +stringLenght);
+	  System.out.println("Longest word into the input String is :" +longestWord);
+  }
+  
+	
+//Actual and expected result should be match.
   @Test
   public void testCaseforStringLenght() {
-	  Assert.assertEquals(am.lenght("The cow jumped over the moon"),28);
-	  
+	  int expectedResult=28;
+	  int actuelResult=am.lenght("The cow jumped over the moon");
+	  Assert.assertEquals(actuelResult,expectedResult);
   }
+
+  
+//Actual and expected result should be match.
   @Test
   public void testCaseForLongestWord() {
-	  Assert.assertEquals(am.longestWord("The cow jumped over the moon"),"jumped");
 	  
-	  
-	  
-  }
+	  String expectedResult="jumped";
+	  String actuelResult=am.longestWord("The cow jumped over the moon");
+	  Assert.assertEquals(actuelResult,expectedResult);
+ }
 }
